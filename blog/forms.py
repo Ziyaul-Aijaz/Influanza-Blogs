739a160b669991c40aa312367b33f0dd19f85214
Ziyaul-Aijaz <56488974+Ziyaul-Aijaz@users.noreply.github.com>
2021-11-13 15:30:22 +0530

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,10 @@
 from django import forms 
 from .models import Post,Category
 
-choices=Category.objects.all().values_list('name','name') 
 
 
 choice_list=[]
+choices=Category.objects.all().values_list('name','name') 
 
 for item in choices:  
 	choice_list.append(item)  
