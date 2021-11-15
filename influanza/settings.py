5f13c8f9693ea619ddd4002a50b13682ed682827
Ziyaul-Aijaz <56488974+Ziyaul-Aijaz@users.noreply.github.com>
2021-11-15 14:54:16 +0530

    Update settings.py

--- influanza/settings.py
+++ influanza/settings.py
@@ -80,6 +80,12 @@ WSGI_APPLICATION = 'influanza.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 import dj_database_url
 db_from_env=dj_database_url.config(conn_max_age=600)
